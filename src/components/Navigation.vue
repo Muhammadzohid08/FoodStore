6af1851e9532b7335882ff434d0330b84e03d301
Muhammadzohid08 <boronovmuhammadzohid50@gmail.com>
2025-07-02 15:48:23 +0500

    ended the top spot of navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="w-full h-auto flex justify-between items-center bg-[#89C74A]">
+        <div class="w-[50%] h-full flex justify-start items-center px-28 py-4 text-white">
+            <a class="text-[23px] border-r-solid border-r-[2px] border-white pr-3 hover:text-black ease-in-out duration-300" href="#"><i class="fa-solid fa-envelope mr-2"></i>info@example.com</a>
+            <a class="ml-5 me-2 text-[23px] hover:text-black ease-in-out duration-300" href="#"><i class="fa-solid fa-phone text-[19px] "></i> 0000 - 123456789</a>
+        </div>
+        <div class="w-[50%] h-full flex justify-end items-center px-28 py-4 text-white ">
+            <a class="text-[23px] pr-3 hover:text-black ease-in-out duration-300" href="#"><i class="fa-brands fa-x-twitter"></i></a>
+            <a class="ml-5 me-2 text-[23px] hover:text-black ease-in-out duration-300" href="#"><i class="fa-brands fa-square-facebook"></i></a>
+            <a class="ml-5 me-2 text-[23px] hover:text-black ease-in-out duration-300" href="#"><i class="fa-brands fa-pinterest"></i></a>
+            <a class="ml-5 me-2 text-[23px] hover:text-black ease-in-out duration-300" href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+    </div>
+</template>
